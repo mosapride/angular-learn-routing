83c915b0fa3da99df1b11e2f72fca0991b14ebb4
mosapride <mosapride01@gmail.com>
2019-06-03 15:25:27 +0900

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Menu1Component } from './page/menu1/menu1.component';
+import { Menu2Component } from './page/menu2/menu2.component';
+import { Menu3Component } from './page/menu3/menu3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'menu1' , component : Menu1Component},
+  {path : 'menu2' , component : Menu2Component},
+  {path : 'menu3' , component : Menu3Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Menu1Component } from './page/menu1/menu1.component';
+import { Menu2Component } from './page/menu2/menu2.component';
+import { Menu3Component } from './page/menu3/menu3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Menu1Component,
+    Menu2Component,
+    Menu3Component
   ],
   imports: [
     BrowserModule,
